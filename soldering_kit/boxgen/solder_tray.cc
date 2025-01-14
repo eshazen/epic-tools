e3762c200b885a870787661158be111a947336b1
Eric Hazen <ericshazen@gmail.com>
2025-01-14 12:54:14 -0500

    option for small test box

--- soldering_kit/boxgen/solder_tray.cc
+++ soldering_kit/boxgen/solder_tray.cc
@@ -10,10 +10,35 @@
 
 #define SPC 0.5
 
+
+#define SMALL_TEST
+
+
+
+#ifdef SMALL_TEST
+
+static const int TabsA = 3;	// long edge tab count
+static const int TabsB = 3;	// width edge tab count
+static const int TabsC = 3;	// vertical edge tab count
+
+static const double tlen = 3;	/* total length */
+static const double twid = 2;		/* width */
+static const double toff = 0.625;	/* lip width (outer minus inner dim) */
+static const double height = 1;	/* depth of box */
+
+#else
+
+static const int TabsA = 5;	// long edge tab count
+static const int TabsB = 3;	// width edge tab count
+static const int TabsC = 3;	// vertical edge tab count
+
 static const double tlen = 18.125;	/* total length */
 static const double twid = 8.5;		/* width */
 static const double toff = 0.625;	/* lip width (outer minus inner dim) */
 static const double height = 1.25;	/* depth of box */
+
+#endif
+
 static const double thick = 0.125;	/* material thickness (for tabs) */
 static const double kerf = 0.004;	/* kerf allowance for laser cutting */
 //static const double kerf = 0.1;	/* kerf allowance for laser cutting */
@@ -33,32 +58,32 @@ int main( int argc, char *argv[]) {
   Tabs::thick = thick;
 
    // bottom
-   tabbedSide( b, len, wid, 5, -3, 5, -3);
+  tabbedSide( b, len, wid, TabsA, TabsB, TabsA, TabsB);
  
    // upper side
    b->draw( ZeroX, ZeroY);
    b->draw( 0, height+SPC);
-   tabbedSide( b, len, height, 5, 3, -5, 3);
+   tabbedSide( b, len, height, -TabsA, -TabsC, -TabsA, -TabsC);
   
    // lower side
    b->draw( ZeroX, ZeroY);
    b->draw( 0, -(SPC+wid));
-   tabbedSide( b, len, height, -5, 3, 5, 3);
+   tabbedSide( b, len, height, -TabsA, -TabsC, -TabsA, -TabsC);
   
     // left side
     b->draw( ZeroX, ZeroY);
     b->draw( -(SPC+height), 0);
-    tabbedSide( b, height, wid, -3, 3, -3, 3);
+    tabbedSide( b, height, wid, -TabsC, -TabsB, -TabsC, -TabsB);
   
     // right side
     b->draw( ZeroX, ZeroY);
     b->draw( len+SPC, 0);
-    tabbedSide( b, height, wid, -3, 3, -3, 3);
+    tabbedSide( b, height, wid, -TabsC, -TabsB, -TabsC, -TabsB);
 
     // top ring
     b->draw( ZeroX, ZeroY);
     b->draw( len, height+toff+3*SPC);
-    tabbedInside( b, len, wid, -5, -3, -5, -3);
+    tabbedInside( b, len, wid, TabsA, TabsB, TabsA, TabsB);
     b->rotate( 0);
     b->draw( ZeroX, ZeroY);
 
